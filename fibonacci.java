12d1520cdea56803327484927c6299ddda04aaed
MATHEWSEBASTIAN12 <mathewksebastian12@gmail.com>
2026-08-02 23:55:17 +0530

    Rename Main class to fibonacci

--- fibonacci.java
+++ fibonacci.java
@@ -1,7 +1,7 @@
 package Module1;
 import java.util.Scanner;
 
-public class Main {
+public class fibonacci {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
