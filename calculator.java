1d299bcb56c04ee313860309d4ebb3b61f5f3988
MATHEWSEBASTIAN12 <mathewksebastian12@gmail.com>
2026-08-05 22:11:45 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,33 +1,47 @@
 package Module1;
+
 import java.util.Scanner;
 
-public class calculator {
+public class Calculator {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter first number: ");
         int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
         int b = sc.nextInt();
+
+        System.out.print("Enter operator (+, -, *, /): ");
         String op = sc.next();
 
         switch (op) {
+
             case "+":
-                System.out.println(a + b);
+                System.out.println("Answer = " + (a + b));
                 break;
 
             case "-":
-                System.out.println(a - b);
+                System.out.println("Answer = " + (a - b));
                 break;
 
             case "*":
-                System.out.println(a * b);
+                System.out.println("Answer = " + (a * b));
                 break;
 
             case "/":
-                System.out.println(a / b);
+                if (b != 0)
+                    System.out.println("Answer = " + (a / b));
+                else
+                    System.out.println("Cannot divide by zero");
                 break;
 
             default:
                 System.out.println("Invalid Operator");
         }
+
+        sc.close();
     }
 }
