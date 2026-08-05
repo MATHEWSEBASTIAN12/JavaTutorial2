f6fafb71307c746921e19c6f6bfc8b82c680b509
MATHEWSEBASTIAN12 <mathewksebastian12@gmail.com>
2026-08-05 22:15:45 +0530

    Update odd.java

--- odd.java
+++ odd.java
@@ -1,17 +1,24 @@
 package Module1;
+
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter the limit: ");
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
+
             if (i % 2 == 0)
                 continue;
 
             System.out.println(i);
         }
+
+        sc.close();
     }
 }
